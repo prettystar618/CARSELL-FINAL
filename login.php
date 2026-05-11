b0c6230911c7c67d0fc261fb1e7e23a5b476753b
prettystar618 <571402366@qq.com>
2026-05-11 14:57:40 +0800

    Update login.php

--- login.php
+++ login.php
@@ -216,7 +216,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     </div>
 
     <div class="footer">
-        © 2026 Cabbage Car Sale. All rights reserved.
+        © 2026 Cabbage Car Sale.
     </div>
 </body>
 </html>
